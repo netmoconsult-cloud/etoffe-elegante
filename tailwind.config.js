67beb42c50572fd856c848a7da0a023dc8993b68
netmoconsult-cloud <netmo.consult@gmail.com>
2026-05-16 15:17:45 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
@@ -10,6 +13,14 @@ export default {
       fontFamily: {
         'sans': ['Inter', 'sans-serif'],
         'serif': ['Playfair Display', 'serif'],
+      },
+      screens: {
+        'xs': '475px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
       },
     },
   },
